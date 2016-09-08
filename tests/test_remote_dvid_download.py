03d415cefccfadef4c927ac15853a1ffcc787196
Jordan Matelsky <j6k4m8@gmail.com>
2016-09-08 17:04:36 -0400

    Don't run if no DVID url

--- tests/test_remote_dvid_download.py
+++ tests/test_remote_dvid_download.py
@@ -8,21 +8,30 @@ FIB25_HASH = '5cc94d532799484cb01788fcdb7cd9f0'
 class TestDVID(unittest.TestCase):
 
     def setUp(self):
-        self.dvid = dvid(os.environ['DVID_TEST_URL'])
+        if os.environ.get('DVID_TEST_URL'):
+            self.dvid = dvid(os.environ['DVID_TEST_URL'])
+        else:
+            self.should_run = False
 
     def test_get_repositories(self):
+        if not self.should_run:
+            return
         self.assertTrue(
             # We know the UUID of the repository sent with default DVID:
             FIB25_HASH in self.dvid.get_repositories()
         )
 
     def test_get_repo_info(self):
+        if not self.should_run:
+            return
         self.assertEqual(
             type(self.dvid.get_repository_info(FIB25_HASH)),
             dict
         )
 
     def test_get_repo_nodes(self):
+        if not self.should_run:
+            return
         self.assertEqual(
             type(self.dvid.get_repository_nodes(FIB25_HASH)),
             list
