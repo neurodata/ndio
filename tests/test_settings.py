647a29941033987c573abd56a139af685fa5efd4
Alex Eusman <Aeusman@users.noreply.github.com>
2017-03-05 15:34:15 -0500

    Update .travis.yml (#263)
    * Update .travis.yml with ndstore instance

---
+++ tests/test_settings.py
@@ -0,0 +1,15 @@
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import sys
+
+DEV_MODE = True
+
+# server to check against
+# SITE_HOST = 'localhost:8080'
+HOSTNAME = 'localhost/nd'
