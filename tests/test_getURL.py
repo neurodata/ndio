e7e18efa8456a121288797c76f1ef91a028a518f
jyshin93 <jyshin93@hotmail.com>
2017-03-06 15:44:09 -0500

    add test_getURL.py

--- tests/test_getURL.py
+++ tests/test_getURL.py
@@ -5,48 +5,60 @@ neurodata_token = "1524d74ec9a915536061af8fd5b51f2cde7de9a5"
 test_token = "b1876fab697e519e0cfe4acc9ca0496bf2889e17"
 Default_token = ""
 
-
 class TestgetURL(unittest.TestCase):
 
     def setUp(self):
-        self.neurodata = neurodata(user_token=neurodata_token)
-        self.public = neurodata(user_token="")
-        self.test = neurodata(user_token=Default_token)
-        self.private_URL = "https://localhost/nd/sd/private_neuro/info/"
-        self.public_URL = "https://localhost/nd/sd/temp_neuro/info/"
-
+        self.neurodata = neurodata(user_token = neurodata_token)
+        self.public = neurodata()
+        self.test = neurodata(user_token = Default_token)
+        self.private_project = "https://localhost/nd/sd/private_neuro/info/"
+        self.public_project = "https://localhost/nd/sd/public_neuro/info/"
+        self.private_test_project = "https://localhost/nd/sd/private_test_neuro/info/"
     def test_masterToken(self):
         try:
-            req = self.neurodata.getURL(self.private_URL)
+            req = self.neurodata.getURL(self.private_project)
+            print(req.content)
             self.assertEqual(req.status_code, 200)
         except ValueError as e:
-            print(e)
+            print(req.content)
         try:
-            req2 = self.neurodata.getURL(self.public_URL)
+            req2 = self.neurodata.getURL(self.public_project)
             self.assertEqual(req2.status_code, 200)
         except ValueError as e:
             print(e)
-
+            
+        try:
+            req3 = self.neurodata.getURL(self.private_test_project)
+            self.assertEqual(req3.status_code, 200)
+        except ValueError as e:
+            print(e)
+    
     def test_testToken(self):
         try:
-            req = self.neurodata.getURL(self.private_URL)
+            req = self.test.getURL(self.private_project)
             self.assertEqual(req.status_code, 403)
         except ValueError as e:
             print(e)
         try:
-            req2 = self.neurodata.getURL(self.public_URL)
+            req2 = self.test.getURL(self.public_project)
             self.assertEqual(req2.status_code, 200)
         except ValueError as e:
             print(e)
+        try:
+            req3 = self.test.getURL(self.private_test_project)
+            self.assertEqual(req3.status_code, 200)
+        except ValueError as e:
+            print(e)
+
 
     def test_publicToken(self):
         try:
-            req = self.neurodata.getURL(self.private_URL)
+            req = self.public.getURL(self.private_project)
             self.assertEqual(req.status_code, 403)
         except ValueError as e:
-            print(e)
+            print(req.content)
         try:
-            req2 = self.neurodata.getURL(self.public_URL)
+            req2 = self.public.getURL(self.public_project)
             self.assertEqual(req2.status_code, 200)
         except ValueError as e:
             print(e)
