8ea52c79f1cd7c87ad3fe8ecb3ad9fd1f1db89a6
Ubuntu <ubuntu@openconnecto.me>
2017-03-06 21:00:14 +0000

    Forcing use of hostname from settings

--- tests/test_getURL.py
+++ tests/test_getURL.py
@@ -1,19 +1,21 @@
 import unittest
 import ndio.remote.neurodata as neurodata
+import test_settings
 
 neurodata_token = "1524d74ec9a915536061af8fd5b51f2cde7de9a5"
 test_token = "b1876fab697e519e0cfe4acc9ca0496bf2889e17"
 Default_token = ""
+HOSTNAME = hostname=test_settings.HOSTNAME
 
 class TestgetURL(unittest.TestCase):
 
     def setUp(self):
-        self.neurodata = neurodata(user_token = neurodata_token)
-        self.public = neurodata()
-        self.test = neurodata(user_token = Default_token)
-        self.private_project = "https://localhost/nd/sd/private_neuro/info/"
-        self.public_project = "https://localhost/nd/sd/public_neuro/info/"
-        self.private_test_project = "https://localhost/nd/sd/private_test_neuro/info/"
+        self.neurodata = neurodata(user_token = neurodata_token, hostname=HOSTNAME)
+        self.public = neurodata(hostname=HOSTNAME)
+        self.test = neurodata(user_token = Default_token, hostname=HOSTNAME)
+        self.private_project = "https://{}/sd/private_neuro/info/".format(HOSTNAME)
+        self.public_project = "https://{}/sd/public_neuro/info/".format(HOSTNAME)
+        self.private_test_project = "https://{}/sd/private_test_neuro/info/".format(HOSTNAME)
     def test_masterToken(self):
         try:
             req = self.neurodata.getURL(self.private_project)
