cf9267e02e1699737264a71f29c729105ec9e713
Cyrob <cyrob.kharse@gmail.com>
2017-05-17 09:24:37 -0700

    debugging setup script

--- ndingest_test_setup.sh
+++ ndingest_test_setup.sh
@@ -1,18 +1,18 @@
 pwd
-if [[ -d /tmp/ndiotest01/ ]]; then
+if [ -d /tmp/ndiotest01/ ]; then
   rm -r /tmp/ndiotest01/
   echo "wow"
 fi
-if [[ -d /tmp/ndiotest02/ ]]; then
+if [ -d /tmp/ndiotest02/ ]; then
   rm -r /tmp/ndiotest02/
   echo "wow"
 
 fi
-if [[ -d /tmp/ndiotest03/ ]]; then
+if [ -d /tmp/ndiotest03/ ]; then
   rm -r /tmp/ndiotest03/
   echo "wow"
 
 fi
 cp -r ./tests/data/test_ingest_pics/. /tmp/
 
-sudo python ./add_to_site_enabled.py
+sudo python ./add_to_sites_enabled.py
